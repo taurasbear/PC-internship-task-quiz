6e2ea2449f8a700da58ca1f09ef5ef47c9c7aead
taurasbear <youngicorn@gmail.com>
2025-01-12 13:24:24 +0200

    Created Infrastructure project. Installed EF Core and EF Core In Memory packages. Implemented QuizContext (set up tables, relationships, InMemory database)

---
+++ PC.Quiz.Infrastructure/Data/DbContext/QuizContext.cs
@@ -0,0 +1,47 @@
+﻿namespace PC.Quiz.Infrastructure.Data.DbContext
+{
+    using Microsoft.EntityFrameworkCore;
+    using PC.Quiz.Domain.Entities;
+
+    public class QuizContext : DbContext
+    {
+        public QuizContext(DbContextOptions options)
+        : base(options) { }
+
+        public DbSet<Entry> Entries { get; set; }
+
+        public DbSet<EntryAnswer> EntryAnswers { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<AnswerOption> AnswerOptions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("QuizDb");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EntryAnswer>()
+                .HasOne(ea => ea.Entry)
+                .WithMany(e => e.EntryAnswers)
+                .HasForeignKey(ea => ea.EntryId);
+
+            modelBuilder.Entity<EntryAnswer>()
+                .HasOne(ea => ea.Question)
+                .WithMany(q => q.EntryAnswers)
+                .HasForeignKey(ea => ea.QuestionId);
+
+            modelBuilder.Entity<EntryAnswer>()
+                .HasOne(ea => ea.AnswerOption)
+                .WithMany(ao => ao.EntryAnswers)
+                .HasForeignKey(ea => ea.AnswerOptionId);
+
+            modelBuilder.Entity<AnswerOption>()
+                .HasOne(ao => ao.Question)
+                .WithMany(q => q.AnswerOptions)
+                .HasForeignKey(ao => ao.QuestionId);
+        }
+    }
+}
