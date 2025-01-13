bb52a45170615b178ec5e3a541b6bcfa72a2c6dd
taurasbear <youngicorn@gmail.com>
2025-01-13 15:29:54 +0200

    Cleaned up client project by deleting unnecessary and adding neccesary files. Installed Material UI. Created page files.

---
+++ pc.quiz.client/src/pages/Quiz.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+const Quiz = () => {
+    return (
+        <Box>
+            Welcome! This is the quiz taking page.
+        </Box>
+    );
+}
+
+export default Quiz;
