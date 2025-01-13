192005f2ae890511c39f217b04978aa8d106dfc1
taurasbear <youngicorn@gmail.com>
2025-01-13 15:28:07 +0200

    Created service extensions in Infrastructure project. Added service extensions in Program.cs.

---
+++ PC.Quiz.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace PC.Quiz.Infrastructure
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using PC.Quiz.Infrastructure.Data.DbContext;
+
+    public static class ServiceExtensions
+    {
+        public static void ConfigureInfrastructure(this IServiceCollection service)
+        {
+            service.AddDbContext<QuizContext>();
+        }
+    }
+}
--- PC.Quiz.Server/Program.cs
+++ PC.Quiz.Server/Program.cs
@@ -1,5 +1,6 @@
 using PC.Quiz.Infrastructure.Data.DbContext;
 using PC.Quiz.Application;
+using PC.Quiz.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.ConfigureApplication();
+builder.Services.ConfigureInfrastructure();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
