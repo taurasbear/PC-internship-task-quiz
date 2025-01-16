9405229d478e1d5eb1026edd57d98bcf200beef3
taurasbear <youngicorn@gmail.com>
2025-01-16 12:35:41 +0200

    Added 'this' keyword to Validator.

--- PC.Quiz.Application/Features/EntryAnswerFeatures/AddEntryAnswers/AddEntryAnswersValidator.cs
+++ PC.Quiz.Application/Features/EntryAnswerFeatures/AddEntryAnswers/AddEntryAnswersValidator.cs
@@ -7,18 +7,18 @@
     {
         public AddEntryAnswersValidator()
         {
-            RuleFor(entryAnswers => entryAnswers.entryAnswers)
+            this.RuleFor(entryAnswers => entryAnswers.entryAnswers)
                 .NotEmpty()
                 .WithMessage("EntryAnswers list must not be empty.");
 
-            RuleForEach(entryAnswers => entryAnswers.entryAnswers)
+            this.RuleForEach(entryAnswers => entryAnswers.entryAnswers)
                 .SetValidator(new EntryAnswerRequestValidator());
 
-            RuleFor(entryAnswers => entryAnswers.entryAnswers)
+            this.RuleFor(entryAnswers => entryAnswers.entryAnswers)
                 .Must(entryAnswers => entryAnswers.Select(ea => ea.QuestionId).Distinct().Count() == 1)
                 .WithMessage("EntryAnswers cannot have distinct QuestionId.");
 
-            RuleFor(entryAnswers => entryAnswers.entryAnswers)
+            this.RuleFor(entryAnswers => entryAnswers.entryAnswers)
                 .Must(entryAnswers => entryAnswers.Select(ea => ea.EntryId).Distinct().Count() == 1)
                 .WithMessage("EntryAnswers cannot have distinct EntryId.");
         }
@@ -28,24 +28,24 @@
     {
         public EntryAnswerRequestValidator()
         {
-            RuleFor(entryAnswer => entryAnswer.QuestionId)
+            this.RuleFor(entryAnswer => entryAnswer.QuestionId)
                 .GreaterThanOrEqualTo(ValidationConstants.MinId)
                 .WithMessage($"QuestionId must greater than or equal to {ValidationConstants.MinId}.");
 
-            RuleFor(entryAnswer => entryAnswer.EntryId)
+            this.RuleFor(entryAnswer => entryAnswer.EntryId)
                 .GreaterThanOrEqualTo(ValidationConstants.MinId)
                 .WithMessage($"EntryId must greater than or equal to {ValidationConstants.MinId}.");
 
-            RuleFor(entryAnswer => entryAnswer.NormalizedValue)
+            this.RuleFor(entryAnswer => entryAnswer.NormalizedValue)
                 .Must(value => value == null || !string.IsNullOrWhiteSpace(value))
                 .WithMessage("NormalizedValue must be null or a non-empty string.");
 
-            RuleFor(entryAnswer => entryAnswer.AnswerOptionId)
+            this.RuleFor(entryAnswer => entryAnswer.AnswerOptionId)
                 .GreaterThanOrEqualTo(ValidationConstants.MinId)
                 .When(entryAnswer => entryAnswer.AnswerOptionId.HasValue)
                 .WithMessage($"AnswerOptionId great than or equal to {ValidationConstants.MinId} if provided.");
 
-            RuleFor(entryAnswer => entryAnswer)
+            this.RuleFor(entryAnswer => entryAnswer)
                 .Must(entryAnswer => !string.IsNullOrWhiteSpace(entryAnswer.NormalizedValue) || entryAnswer.AnswerOptionId.HasValue)
                 .WithMessage("Either NormalizedValue or AnswerOptionId must be provided.");
         }
